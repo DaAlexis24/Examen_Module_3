154d5638b3c9679f12febaeab6831ff2bc6cb7ed
Daniel Soledad <danielalsole24@gmail.com>
2026-05-26 12:16:05 +0200

    feat: Test for Header Component

---
+++ src/front/components/header.test.ts
@@ -0,0 +1,25 @@
+import { createHeader } from './header';
+import '@testing-library/jest-dom';
+import { screen } from '@testing-library/dom';
+
+describe('Given a Header Component', () => {
+    describe('When it used createHeader', () => {
+        // Esto es porque cada vez que se usa este componente, se crea un bloque HTML
+        beforeEach(() => {
+            document.body.innerHTML = '';
+            createHeader();
+        });
+
+        test('Then it render logo, title and button', () => {
+            // Act
+            const logo = screen.getByAltText('Logo de la empresa');
+            const title = screen.getByRole('heading', { name: /productos/i });
+            const button = screen.getByRole('button', { name: /add/i });
+
+            // Assert
+            expect(logo).toBeInTheDocument();
+            expect(title).toBeInTheDocument();
+            expect(button).toBeInTheDocument();
+        });
+    });
+});
