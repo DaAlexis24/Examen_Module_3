f2696d80c3a66f020249fcb0ebf5c2823414b762
Daniel Soledad <danielalsole24@gmail.com>
2026-05-26 12:39:41 +0200

    feat: test for create method to api repo

--- src/front/services/api.repo.test.ts
+++ src/front/services/api.repo.test.ts
@@ -3,7 +3,14 @@ import { Product } from '../types/product';
 
 describe('Given a instance of ApiRepo', () => {
     const repo = new ApiRepo();
-    const mockProduct: Product = {} as Product;
+    const mockProduct: Product = {
+        id: 1,
+        name: 'Test Product',
+        price: 9.99,
+        category: 'components',
+        description: 'test',
+        hasPromo: false,
+    };
 
     describe('When the method getProducts is called', () => {
         describe('And fetch response is OK', () => {
@@ -33,6 +40,36 @@ describe('Given a instance of ApiRepo', () => {
             test('Then it reject the promise', async () => {
                 // Act and Assert
                 expect(repo.getProducts()).rejects.toThrow();
+            });
+        });
+    });
+
+    describe('When method createProduct is called', () => {
+        describe('And fetch response is OK', () => {
+            beforeEach(() => {
+                vi.spyOn(globalThis, 'fetch').mockResolvedValue({
+                    ok: true,
+                    json: vi.fn().mockResolvedValueOnce(mockProduct),
+                } as unknown as Response);
+            });
+            test('Then it create a product', async () => {
+                // Act
+                const result = await repo.createProduct(mockProduct);
+                // Arrange
+                expect(result).toEqual(mockProduct);
+            });
+        });
+
+        describe('And fetch response is not OK', () => {
+            beforeEach(() => {
+                // Arrange
+                vi.spyOn(globalThis, 'fetch').mockResolvedValue({
+                    ok: false,
+                } as unknown as Response);
+            });
+            test('Then it reject the promise', async () => {
+                // Act and Assert
+                expect(repo.createProduct(mockProduct)).rejects.toThrow();
             });
         });
     });
